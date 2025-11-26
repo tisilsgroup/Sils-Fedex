1d5da2799b9eff8fb36d932c11e4231b4bda8a18
Jill Arispe <arispe.jill@gmail.com>
2025-11-26 03:43:58 -0300

    v.1.1.1 Se incorpora gitignore para la cadena de Conexión

--- clases/DataManager.Class.php
+++ clases/DataManager.Class.php
@@ -13,9 +13,7 @@ class DataManager
 		$db = null;
 
 		try {
-			$db = new PDO("sqlsrv:Server=192.168.1.10;Database=ProcesoNew_20250813", 'sa', 'Central2012');
-			//$db = new PDO("sqlsrv:Server=192.168.15.47;Database=SILS_Servidor_20240506_original", 'sa', 'Central2012');
-			// $db = new PDO("sqlsrv:Server=192.168.25.220;Database=ProcesoNew", 'odbc_integra', 'Us3rInt3gr4');
+			$db = new PDO("sqlsrv:Server=192.168.25.220;Database=ProcesoNew", 'odbc_integra', 'Us3rInt3gr4');
 			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		} catch (exception $e) {
 			self::$mensajeError = $e->getMessage();
